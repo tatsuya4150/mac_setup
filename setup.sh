b51e6d3629237fdfc8f701290d222ac1e6365be0
山下達也 <yamashitatatsuya@tatsuya-mbp.local>
2020-07-10 10:54:58 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!bin/sh
+
+brew cask install iterm2
+brew cask install visual-studio-code
+brew cask install sequel-pro
+brew cask install xmind
+brew cask install spectacle
+brew cask install alfred
+brew cask install graphiql
+brew cask install flux
+brew cask install docker
+brew cask install zoomus
+brew cask install clipy
+
+brew install tree
+
